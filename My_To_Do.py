76453d0c930e07e6efa588a8767842b62e8901d0
Carbouba <saniboubacar1992@gmail.com>
2025-12-23 17:52:17 +0100

    Debut_du_programme

---
+++ My_To_Do.py
@@ -0,0 +1,15 @@
+def charge_taches():
+    return
+def sauver_taches():
+    return
+
+while True:
+    print("\n\n====== MENU ======")
+    print("\n1. Voir les tâches")
+    print("2. Ajouter une tâche ")
+    print("3. Supprimer une tâche (C'est le défi !")
+    print("4. Vider toute la liste")
+    print("5. Quitter")
+    print("\n====================")
+
+    choix = input("\nFaites votre choix")
