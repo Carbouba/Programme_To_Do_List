e3193e97a704ed611840ffd25063f227bdca28b0
Carbouba <saniboubacar1992@gmail.com>
2025-12-25 09:17:10 +0100

    To_Do_App_V0.1

--- My_To_Do.py
+++ My_To_Do.py
@@ -1,35 +1,62 @@
 # Les fonctions
 def charge_taches():
-    return
-def sauver_taches():
-    return
-def ajouter():
-    print("===== Nouvelle tâche =====")
-    nom_tache = input("Nom de la tâche : ")
-    for tache in taches:
-        if tache in taches:
-            print("La tâche existe déjâ.")
+    try:
+        with open("Cahier_tâches", "r") as f:
+            # for tache in f:
+                tache = f.read()
+                taches.append(tache)
+    except FileNotFoundError:
+        return 0
+    except ValueError:
+        print("❌ Erreur : Aucune tâche ajouté pour le moment !")
 
+def sauver_taches(nouvelle_tache):
+    with open("Cahier_tâches", "a") as f:
+            f.write(f"{nouvelle_tache}\n")
     return
+
+def ajouter():
+    print("\n===== ➕ Ajout d'une nouvelle tâche ➕ =====")
+    nom_tache = input("Nom de la tâche : ")
+    if nom_tache.lower() in taches:
+        print("❌ Erreur : La tâche existe déjâ.")
+    else:
+        taches.append(nom_tache.lower())
+        print("✅ Nouvelle tâche ajouté avec succée !")
+
+        sauver_taches(nom_tache)
+    return
+
 def supprimer():
     return
 def voir_taches():
+    nombre_taches = len(taches)
+    if nombre_taches == 0:
+        print("❌ Erreur : Aucune tâche ajouté pour le moment !")
+    else:
+        print("===== Liste des tâches =====")
+        for tache in taches:
+            print(f"{tache}")
+        print("====================")
     return
 
 # Programme pricipale
 taches = []
+charge_taches()
+
 while True:
     try:
         
         print("\n\n====== MENU ======\n")
         print("1. Voir les tâches")
         print("2. Ajouter une tâche ")
-        print("3. Supprimer une tâche (C'est le défi !")
+        print("3. Supprimer une tâche")
         print("4. Vider toute la liste")
         print("5. Quitter")
         print("====================")
 
-        option = int(input("\nVeuillez choisir une option (1-3) : "))
+        option = int(input("\nVeuillez choisir une option : "))
+        print("\n")
     except ValueError:
         print("\n❌ Erreur : Vous devez entré un  CHIFFRE, pas une lettre.")
         continue
