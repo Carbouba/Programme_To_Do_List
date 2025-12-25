c66d159afa4ae919f47c84623b328c61ffb2fb6c
Carbouba <saniboubacar1992@gmail.com>
2025-12-25 14:19:39 +0100

    To_Do_App_V1.0_Final

--- My_To_Do.py
+++ My_To_Do.py
@@ -1,18 +1,19 @@
 # Les fonctions
 def charge_taches():
     try:
-        with open("Cahier_tâches", "r") as f:
-            # for tache in f:
-                tache = f.read()
-                taches.append(tache)
+        with open("Cahier_taches.txt", "r") as f:
+            for ligne in f:
+                lign_propre = ligne.strip()
+                taches.append(lign_propre)
     except FileNotFoundError:
         return 0
     except ValueError:
         print("❌ Erreur : Aucune tâche ajouté pour le moment !")
 
-def sauver_taches(nouvelle_tache):
-    with open("Cahier_tâches", "a") as f:
-            f.write(f"{nouvelle_tache}\n")
+def sauver_taches():
+    with open("Cahier_taches.txt", "w") as f:
+            for tache in taches:
+                f.write(f"{tache}\n")
     return
 
 def ajouter():
@@ -24,19 +25,40 @@ def ajouter():
         taches.append(nom_tache.lower())
         print("✅ Nouvelle tâche ajouté avec succée !")
 
-        sauver_taches(nom_tache)
+        sauver_taches()
     return
 
 def supprimer():
-    return
+    nombre_taches = len(taches)
+    if nombre_taches == 0:
+        print("❌ Erreur : Aucune tâche ajouté pour le moment !")
+        return
+    else:
+        print("===== Liste des tâches =====")
+        for i, tache in enumerate(taches, start= 1):
+            print(f"N°{i} : {tache.capitalize()}")
+        print("====================")
+    try:
+        tache_supp = int(input("\nVeuillez saisir le numéro de la tache a supprimer : "))
+        index = tache_supp - 1
+        if 0 <= index < len(taches):
+            tache_enlevee = taches.pop(index)        
+            print(f"🗑️ Tâche '{tache_enlevee}' supprimée !")
+            sauver_taches()
+        else:
+            print("❌ Erreur : Ce numéro n'existe pas.")
+    except ValueError:
+        print("\n❌ Erreur : Vous devez entré un  CHIFFRE, pas une lettre.")
+
+
 def voir_taches():
     nombre_taches = len(taches)
     if nombre_taches == 0:
         print("❌ Erreur : Aucune tâche ajouté pour le moment !")
     else:
         print("===== Liste des tâches =====")
-        for tache in taches:
-            print(f"{tache}")
+        for i, tache in enumerate(taches, start= 1):
+            print(f"N°{i} : {tache.capitalize()}")
         print("====================")
     return
 
@@ -51,8 +73,7 @@ while True:
         print("1. Voir les tâches")
         print("2. Ajouter une tâche ")
         print("3. Supprimer une tâche")
-        print("4. Vider toute la liste")
-        print("5. Quitter")
+        print("4. Quitter")
         print("====================")
 
         option = int(input("\nVeuillez choisir une option : "))
